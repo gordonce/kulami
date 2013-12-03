286354edfac55120d2b59b38fc98fb3426381b06
Gordon Martin <gordonmartin1980@gmail.com>
2013-12-03 15:48:49 +0100

    Main entry point moved from Mainframe to GameController

--- kulami/src/kulami/control/GameController.java
+++ kulami/src/kulami/control/GameController.java
@@ -3,18 +3,43 @@
  */
 package kulami.control;
 
+import javax.swing.JFrame;
+
+import kulami.gui.Mainframe;
+
 /**
  * @author gordon
  * 
  */
 public class GameController implements MessageObserver {
 
-	/* (non-Javadoc)
+	private Mainframe mainframe;
+	
+	public GameController() {
+		
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see kulami.control.MessageObserver#inform(java.lang.String)
 	 */
 	@Override
 	public void inform(String message) {
 		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				GameController gameController = new GameController();
+				JFrame mainframe = new Mainframe(gameController);
+			}
+		});
 		
 	}
 
--- kulami/src/kulami/gui/Mainframe.java
+++ kulami/src/kulami/gui/Mainframe.java
@@ -20,12 +20,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import kulami.control.ConnectionData;
+import kulami.control.GameController;
 import kulami.control.ServerAdapter;
+import kulami.game.GameObservable;
 
 /**
  * @author gordon
- *
+ * 
  */
 public class Mainframe extends JFrame {
 
@@ -41,134 +42,122 @@ public class Mainframe extends JFrame {
 	private JScrollPane messageScrollPane;
 	private JTextArea messageTextArea;
 	private JTextField chatTextField;
+
+	private GameController gameController;
+	private GameDisplay gameDisplay;
+	private MessageDisplay messageDisplay;
+	private NewGameDialog newGameDialog;
+	private PlayerDialog playerDialog;
 	
-	private ServerAdapter serverAdapter;
-	public Mainframe() {
+	/**
+	 * The Mainframe is the central GUI element. It depends on a GameController
+	 * to delegate all functionality. It has a GameDisplay element that is
+	 * responsible for displaying the current game.
+	 * 
+	 * @param gameController
+	 */
+	public Mainframe(GameController gameController) {
+		this.gameController = gameController;
 		initGUI();
-		
-		//testing
-		startServerListener();
-	}
-	
-	
-	// test method
-	private void startServerListener() {
-		ConnectionData serverConnectionData = new ConnectionData("localhost", 1234);
-		serverAdapter = new ServerAdapter(serverConnectionData);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		serverAdapter.connectAndListen();
-		
-		new MessageDisplay(serverAdapter, messageTextArea);
-		
+		setVisible(true);
 	}
 
-
+	public void initGameDisplay(GameObservable game) {
+		gameDisplay = new GameDisplay(game);
+	}
+	
+	public void initMessageDisplay(ServerAdapter serverAdapter) {
+		messageDisplay = new MessageDisplay(serverAdapter, messageTextArea);
+	}
+	
+	public void showNewGameDialog() {
+		newGameDialog = new NewGameDialog();
+	}
+	
+	public void showNewPlayerDialog() {
+		playerDialog = new PlayerDialog(this);
+	}
+	
 	private void initGUI() {
-		setLayout(new BorderLayout(5,10));
-		
-		
+		setLayout(new BorderLayout(5, 10));
+
 		playerMenu = new JMenu("Spieler");
 		JMenuItem addPlayer = new JMenuItem("Neuer Spieler");
 		addPlayer.addActionListener(new PlayerDialog(this));
 		playerMenu.add(addPlayer);
-		
+
 		gameMenu = new JMenu("Server");
 		gameMenu.add(new JMenuItem("Spiel starten"));
 		gameMenu.add(new JMenuItem("Spiel abbrechen"));
-		JMenuItem sendTestMessage = new JMenuItem("Testnachricht senden");
-		sendTestMessage.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				serverAdapter.sendTestMessage();
-			}
-		});
-		gameMenu.add(sendTestMessage);
-		JMenuItem sendServerMessage = new JMenuItem("Nachricht an Server senden");
+		JMenuItem sendServerMessage = new JMenuItem(
+				"Nachricht an Server senden");
 		sendServerMessage.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				serverAdapter.sendMessage("neuerClient(gordon).");
+				// TODO
 			}
 		});
 		gameMenu.add(sendServerMessage);
-		
+
 		boardMenu = new JMenu("Spielfeld");
 		boardMenu.add(new JMenuItem("Spielfeld erstellen"));
 		boardMenu.add(new JMenuItem("Spielfeld bearbeiten"));
 		boardMenu.add(new JMenuItem("Spielfeld laden"));
-		
+
 		mainMenu = new JMenuBar();
 		mainMenu.add(playerMenu);
 		mainMenu.add(gameMenu);
 		mainMenu.add(boardMenu);
-		
+
 		JPanel leftPanel = new JPanel();
-		leftPanel.setPreferredSize(new Dimension(200,600));
+		leftPanel.setPreferredSize(new Dimension(200, 600));
 		leftPanel.setLayout(new GridLayout(0, 1, 5, 5));
-		
+
 		heroStats = new JLabel("Hero");
 		villainStats = new JLabel("Villain");
-		
+
 		JPanel optionsPanel = new JPanel();
 		optionsPanel.setLayout(new GridLayout(3, 1, 5, 5));
 		previousMovesCheckBox = new JCheckBox("letzte Züge");
 		possibleMovesCheckBox = new JCheckBox("mögliche Züge");
 		boardPossessionCheckBox = new JCheckBox("Plattenbesitz");
-		
+
 		optionsPanel.add(previousMovesCheckBox);
 		optionsPanel.add(possibleMovesCheckBox);
 		optionsPanel.add(boardPossessionCheckBox);
-		
+
 		JPanel messagePanel = new JPanel();
 		messagePanel.setLayout(new BorderLayout(5, 5));
-		
+
 		messageTextArea = new JTextArea();
 		messageTextArea.setEditable(false);
 		messageScrollPane = new JScrollPane(messageTextArea,
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		chatTextField = new JTextField();
-		
+
 		messagePanel.add(messageScrollPane, BorderLayout.CENTER);
 		messagePanel.add(chatTextField, BorderLayout.SOUTH);
-		
+
 		leftPanel.add(heroStats);
 		leftPanel.add(villainStats);
 		leftPanel.add(optionsPanel);
 		leftPanel.add(messagePanel);
-		
+
 		JLabel board = new JLabel("Spielfeld", JLabel.CENTER);
 		board.setPreferredSize(new Dimension(600, 600));
-		
+
 		setJMenuBar(mainMenu);
 		add(board, BorderLayout.CENTER);
 		add(leftPanel, BorderLayout.WEST);
-		
+
 		pack();
 		
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+		setTitle("Kulami");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame mainframe = new Mainframe();
-				mainframe.setTitle("Kulami");
-				mainframe.setLocationRelativeTo(null);
-				mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				mainframe.setVisible(true);
-			}
-		});
 	}
 
 }
