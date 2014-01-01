cca0301d8bf7b8ccf915dc7135011700f20959e6
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-01 15:10:09 +0100

    Started implementing Minimax

--- kulami/src/kulami/game/player/CompPlayer.java
+++ kulami/src/kulami/game/player/CompPlayer.java
@@ -6,27 +6,27 @@ package kulami.game.player;
 import kulami.game.board.GameMap;
 import kulami.game.board.Pos;
 
-
-
-
 /**
  * @author gordon
- *
+ * 
  */
 public class CompPlayer extends Player {
 
 	private KulamiStrategy strategy;
-	
+
 	/**
 	 * @param playerName
-	 * @param owner
+	 * @param colour
+	 *            'r' for red or 'b' for black.
 	 */
-	public CompPlayer(String playerName, char owner, int level) {
-		super(playerName, owner);
+	public CompPlayer(String playerName, char colour, int level) {
+		super(playerName, colour);
 		strategy = new RandomStrategy(level);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see kulami.game.player.Player#makeMove(kulami.game.board.GameMap)
 	 */
 	@Override
--- kulami/src/kulami/game/player/MinimaxStrategy.java
+++ kulami/src/kulami/game/player/MinimaxStrategy.java
@@ -3,6 +3,8 @@
  */
 package kulami.game.player;
 
+import java.util.List;
+
 import kulami.game.board.GameMap;
 import kulami.game.board.Pos;
 
@@ -15,14 +17,15 @@ public class MinimaxStrategy implements KulamiStrategy {
 	/**
 	 * Negative infinity is set to the number of points for all panels plus 1
 	 */
-	private static final int POSITIE_INFINITY = 65;
+	private static final int POSITIVE_INFINITY = 65;
 	/**
 	 * Negative infinity is set to the number of points for all panels plus 1
 	 * negated
 	 */
 	private static final int NEGATIVE_INFINITY = -65;
 	private int level;
-	
+	private Pos savedMove;
+
 	public MinimaxStrategy(int level) {
 		this.level = level;
 	}
@@ -35,19 +38,82 @@ public class MinimaxStrategy implements KulamiStrategy {
 	 */
 	@Override
 	public Pos choosePos(GameMap gameMap) {
-		Pos move = null;
-		int eval = max(1, level);
-		
-		return move;
+		savedMove = null;
+		int eval = max(1, level, gameMap);
+		return savedMove;
 	}
 
-	private int max(int player, int depth) {
+	private int max(int player, int depth, GameMap gameMap) {
+		if (depth == 0 || !hasMarbles(player)) {
+			return evaluate(gameMap);
+		}
 		int maxVal = NEGATIVE_INFINITY;
+		List<Pos> possibleMoves = generatePossibleMoves(player, gameMap);
+		for (Pos move: possibleMoves) {
+			GameMap nextGameMap = performMove(player, move, gameMap);
+			int value = min(-player, depth-1, nextGameMap);
+			if (value > maxVal) {
+				maxVal = value;
+				if (depth == level)
+					savedMove = move;
+			}
+		}
 		return maxVal;
 	}
-	
-	private int min(int player, int depth) {
-		int minVal = POSITIE_INFINITY;
-		return minVal;
+
+	private int min(int player, int depth, GameMap gameMap) {
+		if (depth == 0 || !hasMarbles(player)) {
+			return evaluate(gameMap);
+		}
+		int minVal = POSITIVE_INFINITY;
+		List<Pos> possibleMoves = generatePossibleMoves(player, gameMap);
+		for (Pos move: possibleMoves) {
+			GameMap nextGameMap = performMove(player, move, gameMap);
+			int value = max(-player, depth-1, nextGameMap);
+			if (value < minVal) {
+				minVal = value;
+				if (depth == level)
+					savedMove = move;
+			}
+		}		return minVal;
+	}
+
+	/**
+	 * @param move 
+	 * @param player 
+	 * @param gameMap
+	 * @return
+	 */
+	private GameMap performMove(int player, Pos move, GameMap gameMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param player
+	 * @param gameMap
+	 * @return
+	 */
+	private List<Pos> generatePossibleMoves(int player, GameMap gameMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param gameMap
+	 * @return
+	 */
+	private int evaluate(GameMap gameMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * @param player
+	 * @return
+	 */
+	private boolean hasMarbles(int player) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
