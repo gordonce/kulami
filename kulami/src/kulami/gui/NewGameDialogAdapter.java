c10aef3b276bbe20a9cc01cc1313ca5d983c8c72
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 18:46:35 +0100

    NGDAdapter

--- kulami/src/kulami/gui/NewGameDialogAdapter.java
+++ kulami/src/kulami/gui/NewGameDialogAdapter.java
@@ -4,13 +4,21 @@
 package kulami.gui;
 
 /**
+ * Adapter interface for user input from the <code>NewGameDialog</code>
+ * 
  * @author gordon
  * 
  */
 public interface NewGameDialogAdapter {
 
+	/**
+	 * User clicked connect
+	 */
 	public void connectClicked();
 
+	/**
+	 * User clicked cancel
+	 */
 	public void cancelClicked();
 
 }
