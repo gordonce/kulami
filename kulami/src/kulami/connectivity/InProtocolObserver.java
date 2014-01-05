a07e4103deeba4f30c304a70174ee21406aa776d
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 14:08:58 +0100

    InProtocolObserver javadoc

--- kulami/src/kulami/connectivity/InProtocolObserver.java
+++ kulami/src/kulami/connectivity/InProtocolObserver.java
@@ -1,11 +1,12 @@
-/**
- * 
- */
 package kulami.connectivity;
 
 /**
- * Each Object that wants to handle incoming server messages has to implement
- * this protocol interface.
+ * Each object that wants to handle incoming server messages has to implement
+ * the <code>InProtocolObserver</code> interface.
+ * <p>
+ * The names of the methods roughly correspond to server messages and the
+ * parameters to server message parameters. Question marks in server message
+ * names are replaced by the letter 'Q' in method names.
  * 
  * @author gordon
  * 
@@ -19,6 +20,9 @@ public interface InProtocolObserver {
 
 	/**
 	 * Server sent a message. The message is displayed to the user.
+	 * 
+	 * @param msg
+	 *            the message
 	 */
 	public void message(String msg);
 
@@ -28,18 +32,18 @@ public interface InProtocolObserver {
 	public void spielparameterQ();
 
 	/**
-	 * Server sent the game parameters. (We are player 2.) Create a GameMap, a
-	 * Player, and a Game and start displaying the game.
+	 * Server sent the game parameters. (We are player 2.) Create a
+	 * <code>GameMap</code>, a <code>Player</code>, and a <code>Game</code> and
+	 * start displaying the game.
 	 * 
-	 * @param opponentName
-	 *            The name of the opponent.
-	 * @param colour
-	 *            The player's colour ('b' for black or 'r' for red).
+	 * @param boardCode
+	 *            the 200-character map code.
 	 * @param level
-	 *            The game level (1, 2, or 3)
-	 * @param mapCode
-	 *            The 200-character map code.
-	 * 
+	 *            the game level (0, 1, or 2)
+	 * @param colour
+	 *            the player's colour ('b' for black or 'r' for red).
+	 * @param opponentName
+	 *            the name of the opponent.
 	 */
 	public void spielparameter(String boardCode, int level, char colour,
 			String opponentName);
@@ -48,19 +52,17 @@ public interface InProtocolObserver {
 	 * Server sent name of player 2. (We are player 1). Set the display
 	 * accordingly.
 	 * 
-	 * @param name
+	 * @param opponentName
 	 *            The name of the opponent.
-	 * 
 	 */
 	public void name(String opponentName);
 
 	/**
-	 * Server sent colour. (We are player 1.) The Player object and the Game
-	 * object can now be created and the display adjusted.
+	 * Server sent colour. (We are player 1.) The <code>Player</code> object and
+	 * the <code>Game</code> object can now be created and the display adjusted.
 	 * 
 	 * @param colour
 	 *            'b' for black or 'r' for red.
-	 * 
 	 */
 	public void farbe(char colour);
 
@@ -68,43 +70,41 @@ public interface InProtocolObserver {
 	 * Server sent signal to start the game. The argument is the colour of the
 	 * player who begins.
 	 * 
-	 * @param startingPlayer
+	 * @param colour
 	 *            'b' for black or 'r' for red.
-	 * 
 	 */
 	public void spielstart(char colour);
 
 	/**
-	 * Server complained about an illegal move. The String indicates the reason.
+	 * Server complained about an illegal move. The argument indicates the
+	 * reason.
 	 * 
 	 * @param msg
 	 *            Reason for illegal move.
-	 * 
 	 */
 	public void ungueltig(String msg);
 
 	/**
 	 * Server sent new board in response to a legal move.
 	 * 
-	 * @param mapCode
-	 * 
+	 * @param boardCode
+	 *            new board code
 	 */
 	public void gueltig(String boardCode);
 
 	/**
 	 * Server sent opponent's move.
 	 * 
-	 * @param mapCode
-	 * 
+	 * @param boardCode
+	 *            new board code
 	 */
 	public void zug(String boardCode);
 
 	/**
 	 * Server signaled that the game is over and sends the final points.
 	 * 
-	 * @param pointsBlack
 	 * @param pointsRed
-	 * 
+	 * @param pointsBlack
 	 */
 	public void spielende(int pointsRed, int pointsBlack);
 
@@ -112,14 +112,19 @@ public interface InProtocolObserver {
 	 * Opponent sent a message via the server.
 	 * 
 	 * @param msg
-	 * 
 	 */
 	public void playerMessage(String msg);
 
+	/**
+	 * Server sent an unknown message. The parameter contains the message.
+	 * 
+	 * @param msg
+	 *            unknown message text
+	 */
 	public void unknownMessage(String msg);
 
 	/**
-	 * 
+	 * An error occurred while communicating with the server.
 	 */
 	public void connectionError();
 }
