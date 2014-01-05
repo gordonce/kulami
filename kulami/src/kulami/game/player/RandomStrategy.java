1e2d20f706301b45c7588b594a7a103ab78a2636
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 17:22:13 +0100

    RandomStrategy javadoc

--- kulami/src/kulami/game/player/RandomStrategy.java
+++ kulami/src/kulami/game/player/RandomStrategy.java
@@ -11,19 +11,30 @@ import kulami.game.board.GameMap;
 import kulami.game.board.Pos;
 
 /**
+ * This implementation of <code>KulamiStrategy</code> chooses a random position
+ * out of all possible positions.
+ * 
  * @author gordon
- *
+ * 
  */
 public class RandomStrategy implements KulamiStrategy {
 
 	private Random randomGen;
-	
+
+	/**
+	 * The constructor ignores the level.
+	 * 
+	 * @param level
+	 */
 	public RandomStrategy(int level) {
 		randomGen = new Random();
 	}
 
-	/* (non-Javadoc)
-	 * @see kulami.game.KulamiStrategy#choosePos(kulami.game.GameMap, kulami.game.Player)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see kulami.game.KulamiStrategy#choosePos(kulami.game.GameMap,
+	 * kulami.game.Player)
 	 */
 	@Override
 	public Pos choosePos(Game game) {
