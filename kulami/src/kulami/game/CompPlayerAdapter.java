07d0fce0cf644f4d46c85ce5fe185d0515c5531e
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 14:44:49 +0100

    CompPlayerAdapter javadoc

--- kulami/src/kulami/game/CompPlayerAdapter.java
+++ kulami/src/kulami/game/CompPlayerAdapter.java
@@ -6,9 +6,18 @@ package kulami.game;
 import kulami.game.board.Pos;
 
 /**
+ * The <code>CompPlayerAdapter</code> interface is implemented by classes that
+ * want to be notified when a <code>CompPlayer</code> has chosen its next move.
+ * 
  * @author gordon
- *
+ * 
  */
 public interface CompPlayerAdapter {
+	/**
+	 * The <code>CompPlayer</code> has chosen to place a marble at position
+	 * <code>pos</code>.
+	 * 
+	 * @param pos
+	 */
 	public void madeMove(Pos pos);
 }
