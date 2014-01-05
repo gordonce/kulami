256df4b57c94b8a82c1257d93ed6139f017fd225
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 14:12:06 +0100

    MessageObserver javadoc

--- kulami/src/kulami/connectivity/MessageObserver.java
+++ kulami/src/kulami/connectivity/MessageObserver.java
@@ -1,21 +1,25 @@
-/**
- * 
- */
 package kulami.connectivity;
 
 /**
+ * Each object that wants to receive incoming server messages from the
+ * <code>ServerProxy</code> has to implement the <code>MessageObserver</code>
+ * interface.
+ * 
  * @author gordon
- *
+ * 
  */
 public interface MessageObserver {
 
 	/**
+	 * Called when a new message was received from the server.
+	 * 
 	 * @param message
+	 *            the message text
 	 */
 	void inform(String message);
 
 	/**
-	 * 
+	 * Called when an error occurred while listening to the server.
 	 */
 	void connectionError();
 
