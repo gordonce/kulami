aef0a5ca1428bd10abf67e5f616f69a23e8f9a27
Gordon Martin <gordonmartin1980@gmail.com>
2014-01-05 18:56:37 +0100

    PlayerDialogAdapter javadoc

--- kulami/src/kulami/gui/PlayerDialogAdapter.java
+++ kulami/src/kulami/gui/PlayerDialogAdapter.java
@@ -3,16 +3,26 @@
  */
 package kulami.gui;
 
-
 /**
+ * Adapter interface for user input from the <code>PlayerDialog</code>
+ * 
  * @author gordon
  * 
  */
 public interface PlayerDialogAdapter {
 
+	/**
+	 * User clicked OK button
+	 */
 	public void okPressed();
 
+	/**
+	 * User clicked cancel button
+	 */
 	public void cancelPressed();
 
+	/**
+	 * User closed dialog window
+	 */
 	public void windowClosed();
 }
